fbe1205ed8a30a7d9ff47c04876442dba36193fe
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-06-08 21:24:25 +0330

    development singup form page

---
+++ src/app/signup/page.js
@@ -0,0 +1,7 @@
+import SignupPage from "@/template/SignupPage";
+
+function Singup() {
+  return <SignupPage />;
+}
+
+export default Singup;
---
+++ src/components/template/SignupPage.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import styles from "@/template/SignupPage.module.css";
+import Link from "next/link";
+
+function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  return (
+    <div className={styles.form}>
+      <h4>فرم ثبت نام</h4>
+      <form>
+        <label>ایمیل:</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>رمز عبور:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label>تکرار رمز عبور:</label>
+        <input
+          type="password"
+          value={rePassword}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+        <button type="submit">ثبت نام</button>
+      </form>
+      <p>
+        حساب کاربری دارید؟
+        <Link href="/signin">ورود</Link>
+      </p>
+    </div>
+  );
+}
+export default SignupPage;
