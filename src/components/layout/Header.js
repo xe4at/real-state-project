68593e9e52525f78bbf9cc181fd286adf2775e37
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-06-11 19:55:28 +0330

    Client-side header authentication

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,14 @@
-import { FiLogIn } from "react-icons/fi"; // ← اصلاح شد
-import styles from "@/layout/Header.module.css";
+"use client";
+
 import Link from "next/link";
+import { FiLogIn } from "react-icons/fi";
+import { FaUserAlt } from "react-icons/fa";
+import styles from "@/layout/Header.module.css";
+import { useSession } from "next-auth/react";
 
 function Header() {
+  const { data } = useSession();
+
   return (
     <header className={styles.header}>
       <div>
@@ -11,17 +17,24 @@ function Header() {
             <Link href="/">صفحه اصلی</Link>
           </li>
           <li>
-            <Link href="/buy-residential">آگهی‌ها</Link>
-            
+            <Link href="/buy-residential">آگهی ها</Link>
           </li>
         </ul>
       </div>
-      <div className={styles.login}>
-        <Link href="/signin">
-          <FiLogIn style={{ marginLeft: "5px" }} />
-          <span>ورود</span>
-        </Link>
-      </div>
+      {data ? (
+        <div className={styles.login}>
+          <Link href="/dashboard">
+            <FaUserAlt />
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.login}>
+          <Link href="/signin">
+            <FiLogIn />
+            <span>ورود</span>
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
